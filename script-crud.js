d17edcfbcf219654301cc3b97aee92a70a3fe839
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-06-19 16:32:58 -0300

    inserção de funcionalidade aos botoes de editar e cancelar tarefa

--- script-crud.js
+++ script-crud.js
@@ -4,8 +4,13 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
@@ -24,6 +29,17 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
+
+    botao.onclick = () => {
+        debugger
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+        console.log('Nova descrição da tarefa:', novaDescricao);
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    }
 
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', '/imagens/edit.png');
@@ -48,7 +64,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa);
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    atualizarTarefas();
     textArea.value = '';
     formAdicionarTarefa.classList.add('hidden');
 });
@@ -57,3 +73,10 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 })
+
+const limparFormulario = () => {
+    textArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+}
+
+btnCancelar.addEventListener('click',limparFormulario);
