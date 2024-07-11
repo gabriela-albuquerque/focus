fe370006e49d20da2424358429d835d0a8358625
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-07-11 16:16:57 -0300

    ajuste tem foco

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
-tempoDecorridoEmSegundos = 5;
+tempoDecorridoEmSegundos = 1500;
 alterarContexto('foco');
 focoBt.classList.add('active');
 })
